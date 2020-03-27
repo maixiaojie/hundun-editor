9ebe54c313d83b25f29a286420b4f9de64f89d47
wenyujie <wenyujie@hundun.cn>
2020-03-27 18:44:23 +0800

    🎉  vue富文本编辑器初始化

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="hundun-editor-app-wrapper">
+    <div class="hundun-editor-tool-box">
+      <div class="hundun-editor-tool-box-item">正文</div>
+      <div class="hundun-editor-tool-box-item">图片</div>
+    </div>
+    <div class="hundun-editor-content" ref="editorRef" contenteditable="true"></div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default Vue.extend({
-  name: 'App',
-  components: {
-    HelloWorld
+import { defineComponent, ref, onMounted } from "@vue/composition-api";
+export default defineComponent({
+  components: {},
+  setup() {
+    const editorRef = ref<HTMLElement>("");
+    onMounted(() => {
+      editorRef.value.onkeydown = function(e: KeyboardEvent) {
+        if (e.keyCode === 13) {
+          // 回车插入段落
+          console.log(1)
+          document.execCommand("insertHTML", false, "<p><br></p>");
+        }
+      };
+    });
+    return {
+      editorRef
+    };
   }
-})
+});
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+@import "./app.scss";
 </style>
